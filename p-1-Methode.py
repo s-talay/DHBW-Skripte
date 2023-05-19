aab7ee4f5139fd5fc7a05654b0ae0baba2000748
Talay <seyfullah.talay@mannheim.de>
2023-05-19 21:17:38 +0200

    p-1 Methode

--- p-1-Methode.py
+++ p-1-Methode.py
@@ -1,5 +1,4 @@
-primlist = []
-#fertig
+
 def ist_prim(n):
     if n < 2:
         return False
@@ -8,32 +7,34 @@ def ist_prim(n):
             return False
     return True
 
-#todo
-def finde_primezahlen(n):
-    primes = []
-    if n < 2:
-        return primes
-    try:
-        with open('primes.txt', 'r') as f:
-            for line in f:
-                primes.append(int(line.strip()))
-    except FileNotFoundError:
-        pass  # File doesn't exist yet
-    start = primes[-1] + 1 if primes else 2
-    for i in range(start, n+1):
-        if ist_prim(n):
-            primes.append(i)
-    
-    # Save updated list of primes to file
-    with open('primes.txt', 'w') as f:
-        for p in primes:
-            f.write(str(p) + '\n')
-    
-    return primes
+def ggT(a,b):
+    while b:
+        a,b = b, a%b
+    return a
 
-#todo
-def liste_prim_potenzen(n):
-    list = []
-    for i in range(2, n):
-        return
-finde_primezahlen(999999999999)
+def findeFaktor(n):
+    a = 2
+    exp = 2
+    while(n%2 == 0):
+        return 2
+    while(True):
+        g = ggT(a**exp - 1,n)
+        #print("a: ",a)
+        #print("exp: ,",exp)        
+        if(g > 1):
+            print("Faktor gefunden: ",g)
+            return g
+        exp+=1
+
+def pMinus1(n):
+    faktoren = []
+    numb = n
+    while(True):
+        faktor = findeFaktor(numb)
+        faktoren.append(faktor)
+        numb = int(numb/faktor)
+        print("numb: ",numb)
+        print("faktor: ",faktor)
+        if(numb == 1):
+            return faktoren
+
--- test.py
+++
@@ -1,3 +0,0 @@
-def test(inval):
-    outval = ((inval & 0xe0) >> 4) | ((inval & 0x1c) << 3) | ((inval & 0x02) >> 1) | ((inval & 0x01) << 4)
-    return outval
