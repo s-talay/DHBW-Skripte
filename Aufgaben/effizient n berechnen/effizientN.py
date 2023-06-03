94756b0a8edbc02cd0ac38f8e7d238c6bfc5d7e3
KingSeyfo <seyfullahtalay@outlook.de>
2023-06-03 02:15:36 +0200

    amogus

---
+++ Aufgaben/effizient n berechnen/effizientN.py
@@ -0,0 +1,11 @@
+def iteriertesQuad(basis,exp,mod):
+    if(exp == 0):
+        return 1
+    if(exp%2 == 0):
+        result = iteriertesQuad(basis,exp/2,mod)
+        return (result*result)%mod
+    else:
+        result = iteriertesQuad(basis,exp-1,mod)
+        return (basis*result)%mod
+    
+    
